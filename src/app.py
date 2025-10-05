6ed252bea423caeeee02b8618efa769a37009c25
Priska <priskaolla05@gmail.com>
2025-10-05 20:23:40 +0700

    add vulnerable function run_commad with shell=True

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def add(a, b):
     return a + b
 
@@ -5,3 +7,9 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Tidak boleh bagi nol")
     return a / b
+
+def run_command(cmd):
+    # BUG: penggunaan shell=True berbahaya (Command Injection)
+    result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
+    return result.stdout
+
